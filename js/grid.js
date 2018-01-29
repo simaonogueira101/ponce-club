8657767c8e34055d952a06c5b25aed1326f8ab75
simaonogueira101 <simao.nogueira66@gmail.com>
2018-01-29 23:02:28 +0000

    Adds js to the website.

---
+++ js/grid.js
@@ -0,0 +1,7 @@
+$('main').masonry({
+  // options
+  itemSelector: 'section',
+  columnWidth: 400,
+  transitionDuration: 0,
+  gutter: 64,
+});
---
+++ js/looper.js
@@ -0,0 +1,55 @@
+let myFlooper1 = new flooper('.js-flooper1', {
+  autoPlay: true, //
+  flooperItemSelector: '.js-flooper-item', // string class selector
+  bufferSize: 0,
+  name: `first`,
+  speed: 0.5,
+  onFloop: () => {}, //noop
+  onStart: () => {}, //noop
+  onPlay: () => {}, //noop
+  onPause: () => {}, //noop
+  onSlowmotion: () => {}, //noop
+});
+myFlooper1.init();
+
+let myFlooper2 = new flooper('.js-flooper2', {
+  autoPlay: true, //
+  flooperItemSelector: '.js-flooper-item', // string class selector
+  bufferSize: 0,
+  name: `first`,
+  speed: 0.5,
+  onFloop: () => {}, //noop
+  onStart: () => {}, //noop
+  onPlay: () => {}, //noop
+  onPause: () => {}, //noop
+  onSlowmotion: () => {}, //noop
+});
+myFlooper2.init();
+
+let myFlooper3 = new flooper('.js-flooper3', {
+  autoPlay: true, //
+  flooperItemSelector: '.js-flooper-item', // string class selector
+  bufferSize: 0,
+  name: `first`,
+  speed: 0.5,
+  onFloop: () => {}, //noop
+  onStart: () => {}, //noop
+  onPlay: () => {}, //noop
+  onPause: () => {}, //noop
+  onSlowmotion: () => {}, //noop
+});
+myFlooper3.init();
+
+let myFlooper4 = new flooper('.js-flooper4', {
+  autoPlay: true, //
+  flooperItemSelector: '.js-flooper-item', // string class selector
+  bufferSize: 0,
+  name: `first`,
+  speed: 0.5,
+  onFloop: () => {}, //noop
+  onStart: () => {}, //noop
+  onPlay: () => {}, //noop
+  onPause: () => {}, //noop
+  onSlowmotion: () => {}, //noop
+});
+myFlooper4.init();
